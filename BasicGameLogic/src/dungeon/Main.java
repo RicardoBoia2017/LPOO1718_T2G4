5d10e3f76ef9708985291c9196c56128548ac3b5
Luís Mendes <luis.alvela@gmail.com>
2018-02-14 08:58:12 +0000

    Completed task 1.

--- BasicGameLogic/src/dungeon/Main.java
+++ BasicGameLogic/src/dungeon/Main.java
@@ -3,8 +3,20 @@ package dungeon;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 char [][] matrix = {{'X','X','X','X','X','X','X','X','X', 'X'}, 
+								   {'X','H',' ',' ','I',' ','X',' ','G', 'X'},
+								   {'X','X','X',' ','X','X','X',' ',' ', 'X'},
+								   {'X',' ','I',' ','I',' ','X',' ',' ', 'X'},
+								   {'X','X','X',' ','X','X','X',' ',' ', 'X'},
+								   {'I',' ',' ',' ',' ',' ',' ',' ',' ', 'X'},
+								   {'I',' ',' ',' ',' ',' ',' ',' ',' ', 'X'},
+								   {'X','X','X',' ','X','X','X','X',' ', 'X'},
+								   {'X',' ','I',' ','I',' ','X','k',' ', 'X'},
+								   {'X','X','X','X','X','X','X','X','X', 'X'}};
+		
+		 for(int i = 0; i < matrix.length; i++) {
+			 System.out.println(matrix[i]);
+		 }
 	}
 
 }
