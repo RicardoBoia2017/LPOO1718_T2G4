05777a2f1834b58cc71acfe0c11fa3f7a44995d1
Luís Mendes <luis.alvela@gmail.com>
2018-04-11 10:46:39 +0100

    Trying to add a button

--- Monopoly/core/src/mono/poly/Monopoly.java
+++ Monopoly/core/src/mono/poly/Monopoly.java
@@ -2,18 +2,48 @@ package mono.poly;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class Monopoly extends ApplicationAdapter {
 	SpriteBatch batch;
+    Stage stage;
 	Texture img;
+    TextButton button;
+    TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
 	
 	@Override
 	public void create () {
+		stage = new Stage();
 		batch = new SpriteBatch();
-		img = new Texture("C:/Users/ricar/Desktop/test/core/assets/Monopoly.png");
+		img = new Texture("/home/luis/git/LPOO1718_T2G4/Monopoly/Monopoly/core/assets/Monopoly.png");
+        font = new BitmapFont();
+        skin = new Skin();
+        
+        AssetManager am = new AssetManager();
+        am.load("buttons.pack", TextureAtlas.class);
+        am.finishLoading();
+        buttonAtlas = am.get("buttons.pack");
+        
+        skin.addRegions(buttonAtlas);
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("up-button");
+        textButtonStyle.down = skin.getDrawable("down-button");
+        textButtonStyle.checked = skin.getDrawable("checked-button");
+        button = new TextButton("Button1", textButtonStyle);
+        stage.addActor(button);
 	}
 
 	@Override
@@ -22,6 +52,7 @@ public class Monopoly extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0, 400, 400, 0, 1, 1, 0);
+		stage.draw();
 		batch.end();
 	}
 	
