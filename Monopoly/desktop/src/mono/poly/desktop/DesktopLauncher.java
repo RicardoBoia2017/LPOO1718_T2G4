748087b41aa4a4280f6a9c99f658a49ab717b21d
Ricardo Boia <up201505244@fe.up.pt>
2018-04-11 10:16:09 +0100

    Added Monopoly

---
+++ Monopoly/desktop/src/mono/poly/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package mono.poly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mono.poly.Monopoly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();		
+		config.title = "Monopoly";
+		config.width = 400;
+		config.height = 400;
+		new LwjglApplication(new Monopoly(), config);
+	}
+}
